a6fdcca4a3d4146824a4290246045515def89085
Sean Handley <sean.handley@gmail.com>
2013-06-02 11:14:58 +0100

    Using variables, installing/starting nginx.

---
+++ nginx/recipes/default.rb
@@ -0,0 +1,11 @@
+package "nginx"
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
+template "/etc/nginx/nginx.conf" do
+  notifies :reload, "service[nginx]"
+  variables :user => 'www-data'
+end
